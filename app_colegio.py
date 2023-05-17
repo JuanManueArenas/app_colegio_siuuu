26666c34fc0ea37d30e7a2aee55fcabba2b11423
JuanManueArenas <juanarenas@colegioguanenta.edu.co>
2023-05-17 09:18:28 -0500

    se agrego otra parte del codigo

--- app_colegio.py
+++ app_colegio.py
@@ -14,10 +14,73 @@ def borrar():
     caja_informacion.set("")
     caja_informacion.delete("1.0","end")
 
+# abrir toplevel a notas
+def abrir_toplevel_notas():
+    global toplevel_notas
+    toplevel_notas = Toplevel()
+    toplevel_notas.title("Notas_Madrid")
+    toplevel_notas.resizable(False, False)
+    toplevel_notas.geometry("500x500")
+    toplevel_notas.config(bg="white")
+
+#frame toplevel
+    frame_notas = Frame(toplevel_notas)
+    frame_notas.config(bg="white", width=500, height=500)
+    frame_notas.place(x=0, y=0)
+
+    frame_madri = Frame(toplevel_notas)
+    frame_madri.config(bg="gold2", width=450    , height=450)
+    frame_madri.place(x=0, y=0)
+    
+    lineas_texto_toplevel = Label(toplevel_notas, text = "Procedimental 30%:")
+    lineas_texto_toplevel .config(bg ="gold2", fg="white", font=("Arial", 14))
+    lineas_texto_toplevel .place(x=45, y=25)
+    
+    lineas_texto_toplevel = Label(toplevel_notas, text = "Cognitivo 30%:")
+    lineas_texto_toplevel.config(bg ="gold2", fg="white", font=("Arial", 14))
+    lineas_texto_toplevel.place(x=45, y=65)
+    
+    lineas_texto_toplevel = Label(toplevel_notas, text = "Bimestral 20%:")
+    lineas_texto_toplevel.config(bg ="gold2", fg="white", font=("Arial", 14))
+    lineas_texto_toplevel.place(x=45, y=105)
+
+    lineas_texto_toplevel = Label(toplevel_notas, text = "Autoevaluacion 10%:")
+    lineas_texto_toplevel.config(bg ="gold2", fg="white", font=("Arial", 14))
+    lineas_texto_toplevel.place(x=45, y=145)
+
+    lineas_texto_toplevel = Label(toplevel_notas, text = "Actitudinal 10%:")
+    lineas_texto_toplevel.config(bg ="gold2", fg="white", font=("Arial", 14))
+    lineas_texto_toplevel.place(x=45, y=185)
+
+    caja_informacion_toplevel = Entry(toplevel_notas)
+    caja_informacion_toplevel.config(bg="white", fg="Black", font=("Times New Roman", 15), width=10)
+    caja_informacion_toplevel.focus_set()
+    caja_informacion_toplevel.place(x=250,y=25)
+
+    caja_informacion_toplevel = Entry(toplevel_notas)
+    caja_informacion_toplevel.config(bg="white", fg="Black", font=("Times New Roman", 15), width=10)
+    caja_informacion_toplevel.focus_set()
+    caja_informacion_toplevel.place(x=250,y=65)
+
+    caja_informacion_toplevel = Entry(toplevel_notas)
+    caja_informacion_toplevel.config(bg="white", fg="Black", font=("Times New Roman", 15), width=10)
+    caja_informacion_toplevel.focus_set()
+    caja_informacion_toplevel.place(x=250,y=105)
+
+    caja_informacion_toplevel = Entry(toplevel_notas)
+    caja_informacion_toplevel.config(bg="white", fg="Black", font=("Times New Roman", 15), width=10)
+    caja_informacion_toplevel.focus_set()
+    caja_informacion_toplevel.place(x=250,y=145)
+
+    caja_informacion_toplevel = Entry(toplevel_notas)
+    caja_informacion_toplevel.config(bg="white", fg="Black", font=("Times New Roman", 15), width=10)
+    caja_informacion_toplevel.focus_set()
+    caja_informacion_toplevel.place(x=250,y=185)
 
 
 
 
+#entrada       
 frame_entrada = Frame(ventana_principal)
 frame_entrada.config(bg="DodgerBlue3" , width=1000, height=500)
 frame_entrada.place(x=300, y=0)
@@ -58,19 +121,14 @@ lineas_texto = Label(frame_entrada, text = "Grado:")
 lineas_texto.config(bg ="DodgerBlue3", fg="white", font=("Arial", 10))
 lineas_texto.place(x=45, y=145)
 
-# boton para borrar
-bt_borrar = Button(frame_entrada, text="Borrar", command=borrar)
-bt_borrar.place(x=190, y=35, width=100, height=30)
+#boton para entrar 
+blocksito = PhotoImage(file="img/madri (1).png")
 
+#boton notas
 
-
-
-
-
-
-
-
-
+bt_notas = Button(frame_entrada, image=blocksito, command=abrir_toplevel_notas)
+bt_notas.config(bg="DodgerBlue3", fg="black", font=("Arial", 14))
+bt_notas.place(x=100, y=250)
 
 logo = PhotoImage(file="img/images.png")
 lb_logo = Label(ventana_principal, image=logo, bg="white")
@@ -79,35 +137,5 @@ lb_logo.place(x=0,y=0)
 Imagen = PhotoImage(file="img/Hala (1).png")
 lb_Imagen = Label(frame_entrada, image=Imagen, bg="DodgerBlue3")
 lb_Imagen.place(x=500,y=275)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ventana_principal.mainloop()
